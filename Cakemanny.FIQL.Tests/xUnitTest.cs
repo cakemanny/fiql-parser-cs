b233ffbb93f8a30f23fda71a0c58dec8e0ce4c7e
cakemanny <goldingd89@gmail.com>
2014-10-04 22:17:34 +0100

    initial commit - empty projects

---
+++ Cakemanny.FIQL.Tests/xUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Moq;
+
+namespace Cakemanny.FIQL.Tests
+{
+    public class xUnitTest
+    {
+        public xUnitTest()
+        {
+
+        }
+
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
