83e5b0584c0802126f223aef1cd3844d3919240c
cakemanny <goldingd89@gmail.com>
2014-10-05 11:43:45 +0100

    added in more of the tests we wanted

--- Cakemanny.FIQL.Tests/FIQLParserTest.cs
+++ Cakemanny.FIQL.Tests/FIQLParserTest.cs
@@ -24,5 +24,68 @@ namespace Cakemanny.FIQL.Tests
             string expected = "(timestamp >= '2014-09-29')";
             Assert.That(parser.parseQuery(input), Is.EqualTo(expected));
         }
+
+        [Test]
+        public void ConvertsACommaToAnOR()
+        {
+            string input = "user_id==5,action==money";
+            string expected = "(user_id = 5 OR action = 'money')";
+            Assert.That(parser.parseQuery(input), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ConvertsASemicolonToAnAND()
+        {
+            string input = "user_id==5;action==money";
+            string expected = "(user_id = 5 AND action = 'money')";
+            Assert.That(parser.parseQuery(input), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ErrorsOnUnkownFieldName()
+        {
+            try
+            {
+                parser.parseQuery("unknown_field==10");
+                Assert.Fail("Expected a ParseException to be thrown");
+            }
+            catch (ParseException e)
+            {
+                Assert.That(e.Message, Contains.Substring("not valid fields")
+                        & Contains.Substring("unknown_field"));
+            }
+        }
+
+        [Test]
+        public void ConvertsStarInStringToLIKE()
+        {
+            string input = "action==mon*y";
+            string expected = "(action LIKE 'mon%y')";
+            Assert.That(parser.parseQuery(input), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void CanHandleAnIpAddress()
+        {
+            string input = "ip_address==192.168.0.1";
+            string expected = "(ip_address = '192.168.0.1')";
+            Assert.That(parser.parseQuery(input), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void CanHandleIpv6()
+        {
+            string input = "ip_address==2001:0db8:85a3:0000:0000:8a2e:0370:7334";
+            string expected = "(ip_address = '2001:0db8:85a3:0000:0000:8a2e:0370:7334')";
+            Assert.That(parser.parseQuery(input), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void CanHandleIpv4Like()
+        {
+            string input = "ip_address==192.168.*";
+            string expected = "(ip_address LIKE '192.168.%')";
+            Assert.That(parser.parseQuery(input), Is.EqualTo(expected));
+        }
     }
 }
